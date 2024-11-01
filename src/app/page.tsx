c6d9f872f01ca432e37b3ff5edf419eef399309c
ayeshafahad2 <ayeshafahad661@gmail.com>
2024-11-01 11:30:42 +0500

    ready for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import React from "react";
 import Navbar from "./components/navbar";
 import About from "./aboutus/page";
 import Accessories from "./accessories/page";
 import Review from "./reviews/page";
-import Meet from "./components/footer";
 import Bags from "./components/bags";
 
 
